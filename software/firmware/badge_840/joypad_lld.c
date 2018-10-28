da50293afe5b571be40d7e78a8d30fe3b11255f3
Bill Paul <wpaul@windriver.com>
2018-10-28 02:42:38 -0700

    Give the joypad interrupt handler thread some more stack.

--- software/firmware/badge_840/joypad_lld.c
+++ software/firmware/badge_840/joypad_lld.c
@@ -57,7 +57,7 @@ extern orchard_app_instance instance;
 static void joyInterrupt (EXTDriver *, expchannel_t);
 
 static thread_reference_t joyThreadReference;
-static THD_WORKING_AREA(waJoyThread, 128);
+static THD_WORKING_AREA(waJoyThread, 256);
 
 /* Default state is all buttons pressed. */
 
