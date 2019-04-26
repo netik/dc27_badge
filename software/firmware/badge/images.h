597a5bbe9d78886a0bcef5ff2e08a1e5c384a7e9
John Adams <john@twothink.co>
2019-04-25 22:02:14 -0700

    add images.h with shell logo

---
+++ software/firmware/badge/images.h
@@ -0,0 +1,26 @@
+
+#define SHELL_BANNER "\n     _.-^^---....,,--       \n _--                  --_   \n<                        >) \n|                         | \n \._                   _./  \n    ```--. . , ; .--'''     \n          | |   |           \n       .-=||  | |=-.        \n       `-=#$%&%$#=-'        \n          | ;  :|           \n _____.,-#%&$@%#&#~,._____\n"
+
+#define SHELL_CREDITS "\r\n \
+DEFCON 27 Unofficial Badge Team\r\n \
+\r\n\
+John Adams (pcb, hw, sw)\r\n \
+Bill Paul (hw, sw)\r\n \
+Egan Hirvelda (game mechanics)\r\n"
+
+// defining this here because I don't want to keep looking it up
+// and I want to save calls to getwidth/height
+#define SCREEN_WIDTH  320
+#define SCREEN_HEIGHT 240
+
+// Remember! Filenames must be in old school dos format (8x3)
+//                         12345678.123
+#define IMG_SPLASH         "SPLASH.RGB"
+
+// images used in the app
+#define IMG_UNLOCKBG       "UNLOCKBG.RGB"
+#define IMG_CHOOSETYPE     "CHOOSE.RGB"
+
+#define IMG_SPLASH_DISPLAY_TIME          5000 // longer if music playing
+#define IMG_SPLASH_NO_SOUND_DISPLAY_TIME 1000
+
