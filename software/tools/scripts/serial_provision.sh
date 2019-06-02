08be59e10eb53b1b05e81fb953debeb4b0b7bc72
John Adams <jna@retina.net>
2019-06-01 23:42:11 -0700

    make serial provision script run in loop

--- software/tools/scripts/serial_provision.sh
+++ software/tools/scripts/serial_provision.sh
@@ -2,15 +2,30 @@
 
 echo
 echo "daBomb cp2102 provision Ready."
-echo -n "Connect cable and hit ENTER! -->"
 
-read X
+while [ /bin/true ];
+do
+    
+    echo "Connect cable and hit ENTER or Q to quit."
+    echo -n "==> "
+    
+    read X
+    
+    if [ "$X" == "q" -o "$X" == "Q" ]; then
+        echo "[*] Bye!"
+        exit
+    fi
+    
+    echo "[*] Attempting serial provision for 2102 ..."
+    
+    NEWSERIAL=`uuidgen | sed 's/-//g' | cut -c1-16`
+    ./bin/cp2102 -p "da Bomb" -m "Team Ides" -g on -s ${NEWSERIAL}
+    
 
-echo "[*] Attempting serial provision for 2102 ..."
-
-NEWSERIAL=`uuidgen | sed 's/-//g' | cut -c1-16`
-./bin/cp2102 -p "da Bomb" -m "Team Ides" -g on -s ${NEWSERIAL}
-
-echo "[*] Provisioned serial ${NEWSERIAL}..."
-echo `date` ${NEWSERIAL} >> serials.txt
+    echo "[*] Provisioned serial ${NEWSERIAL}..."
+    echo "[*] Serial number will not be returned by chip until full power cycle."
+    echo
+    echo `date` ${NEWSERIAL} >> serials.txt
+    echo
+done
 
