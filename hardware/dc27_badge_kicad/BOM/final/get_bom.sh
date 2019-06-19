32e1db886653d9cbb53fd614a33dd19ef16f4539
John Adams <john@twothink.co>
2019-06-19 16:38:41 -0700

    Have the getbom script run jsonpp for cleanups after download.
    Check in these downloads so someone else can have them later.

--- hardware/dc27_badge_kicad/BOM/final/get_bom.sh
+++ hardware/dc27_badge_kicad/BOM/final/get_bom.sh
@@ -6,7 +6,7 @@
 # J. Adams <jna@retina.net> 6/2019
 #
 
-# You can pretty-print these json outputs with the jsonpp utility, available via:
+# You will need jsonpp to run this script, available via:
 #    brew install jsonpp
 #
 # set APIKEY in your environment prior to calling this script.
@@ -14,10 +14,16 @@
 
 # get the bill of materials
 curl -H 'Accept: application/json' \
-        https://api.macrofab.com/api/v3/pcb/68v44h8/1/bom\?apikey=$APIKEY > bom.json
+        https://api.macrofab.com/api/v3/pcb/68v44h8/1/bom\?apikey=$APIKEY > out.json
+
+jsonpp out.json > bom.json
+rm out.json
 
 # and then get the positions for the board. For some reason you can't
 # get these in API v3.
 curl -H 'Accept: application/json' \
-        https://api.macrofab.com/api/v2/pcb/68v44h8/1/xyrs\?apikey=$APIKEY > bom.xyrs
+        https://api.macrofab.com/api/v2/pcb/68v44h8/1/xyrs\?apikey=$APIKEY > out.json
+
+jsonpp out.json > bom-xyrs.json
+rm out.json
 
