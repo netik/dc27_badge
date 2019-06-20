e29a685ac13e1db142bd2d3fc3b1a80ee8f4f430
John Adams <jna@retina.net>
2019-06-19 20:23:33 -0700

    Add dual programming script so I can flash two badges at the same time for testing the game.
    Add egan's new maps.

--- software/tools/scripts/mapsplit.sh
+++ software/tools/scripts/mapsplit.sh
@@ -1,2 +1,5 @@
-convert seabattle-4x4-map.tif -crop 320x240 map-%02d.tif
-convert seabattle-4x4-map.tif -resize 320x240 world.tif
+#convert seabattle-4x4-map.tif -crop 320x240 map-%02d.tif
+#convert seabattle-4x4-map.tif -resize 320x240 world.tif
+
+convert eganworld.tif -crop 320x240 map-%02d.tif
+convert eganworld.tif -resize 320x240 world.tif
