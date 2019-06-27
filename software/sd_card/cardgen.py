de4268c81bf79dbdbe9e1c1cf9dfe7e1e38b645d
John-Mark Gurney <jmg@newcontext.com>
2019-06-26 17:31:02 -0700

    make this importable so you can work w/ the shuffled deck

--- software/sd_card/cardgen.py
+++ software/sd_card/cardgen.py
@@ -7,6 +7,8 @@ deck = [ j + i for i in suits for j in cards ] + [ 'FJ', 'SJ' ]
 r = random.Random()
 r.seed(230987459872341398732)
 shuffled = ''.join(r.sample(deck, len(deck)))
-nperrow = 16
-for i in xrange(0, len(shuffled), nperrow):
-	print shuffled[i:i + nperrow]
+
+if __name__ == '__main__':
+	nperrow = 12
+	for i in xrange(0, len(shuffled), nperrow):
+		print shuffled[i:i + nperrow]
