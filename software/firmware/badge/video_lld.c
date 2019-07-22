6c14a72f152372432c20b4b4c31e3e986d143cdd
Bill Paul <wpaul@windriver.com>
2019-07-22 00:06:06 -0700

    Update copyright

--- software/firmware/badge/video_lld.c
+++ software/firmware/badge/video_lld.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2017-2018
+ * Copyright (c) 2017-2019
  *      Bill Paul <wpaul@windriver.com>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
