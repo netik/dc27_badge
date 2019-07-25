7057413d9a452d5143c8d0b6b82153e2db5e2080
John Adams <john@twothink.co>
2019-07-25 13:05:39 -0700

    Remove 'kickstarter' from the provisioning script - we're done with those boards.

--- software/tools/scripts/serial_provision_deviceid.sh
+++ software/tools/scripts/serial_provision_deviceid.sh
@@ -11,7 +11,7 @@ echo "daBomb cp2102 provision Ready."
 while [ /bin/true ];
 do
     
-    /bin/echo "Connect cable and hit ENTER or Q to quit."
+    /bin/echo "Connect cables and hit ENTER or Q to quit."
     /bin/echo -n "==> "
     
     read X
@@ -20,17 +20,42 @@ do
         echo "[*] Bye!"
         exit
     fi
+
+
+    echo "[*] start openocd"
+    X=1
+    BASE_GDB=$(( 3333 + $X - 1 ))
+    BASE_TELNET=$(( 4444 + $X - 1 ))
+    BASE_TCL=$(( 6666 + $X - 1 ))
+    SERIAL="OLAC6A5A"
+
+    openocd -f interface/ftdi/olimex-arm-usb-ocd-h.cfg      \
+        -f interface/ftdi/olimex-arm-jtag-swd.cfg       \
+        -f target/nrf52.cfg                             \
+        -c "gdb_port ${BASE_GDB}" \
+        -c "telnet_port ${BASE_TELNET}" \
+        -c "tcl_port ${BASE_TCL}" \
+        -c "ftdi_serial ${SERIAL}" \
+        -c "gdb_flash_program enable"                   \
+        -c "gdb_breakpoint_override hard"               \
+        -c "nrf52.cpu configure -rtos ChibiOS"  &
     
     echo "[*] Attempting serial provision for 2102 ..."
     
     NEWSERIAL=`arm-none-eabi-gdb -q --batch --command=scripts/deviceid.txt | grep DEVICEID | cut -c10-27`
+    ./bin/cp2102 -l src/cp2102n_default.txt
     ./bin/cp2102 -x 500 -p "da Bomb" -m "Team Ides" -g on -s ${NEWSERIAL}
     
-
     echo "[*] Provisioned serial ${NEWSERIAL}..."
     echo "[*] Serial number will not be returned by chip until full power cycle."
     echo
     echo `date` ${NEWSERIAL} >> serials.txt
     echo
+
+    echo "[*] stop open ocd"
+    pkill openocd
+
+    echo "[*] provision."
+    time ( cd /Users/jna/src/badge/dc27_badge/hardware/programming;  ./FINAL-provision-nrf52-olimex.sh)
 done
 
